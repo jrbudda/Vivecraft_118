e0026d32343953a4939258f7100386ce2fa22cf7
NicBOMB <19939315+NicBOMB@users.noreply.github.com>
2022-07-04 12:51:04 -0700

    Installer Parity on Linux
    Decompile still fails on Linux though

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-python2 install.py $@
+python2 install.py -m mcp_vr $@ # runs installer
+rmdir mcp_vr/lib # delete empty directory
+mkdir -p mcp_nonvr # create nonvr directory
+ln -f -s -r -t mcp_vr lib # linking jars to vr for eclipse
+ln -f -s -r -t mcp_nonvr lib # linking jars to nonvr for eclipse
+cp -R -f mcp_vr/* mcp_nonvr # make nonvr
+python2 applychanges.py -m mcp_nonvr -v NONVR # build
