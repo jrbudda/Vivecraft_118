c9aab89038fbd02639211dfa218362a97938881f
NicBOMB <19939315+NicBOMB@users.noreply.github.com>
2022-07-08 22:15:12 -0700

    update eclipse compiler settings at install
    better conditioning and variable usage as well

--- eclipseclasspath.sh
+++ eclipseclasspath.sh
@@ -7,19 +7,22 @@ if [ $# -lt 1 ]; then
 	echo "No directory to update was provided in argument 1!"
 	exit 1
 fi
-if [ -d "$1/jars/libraries" ] && [ -f "$1/eclipse/Client/.classpath" ]; then
-	ECLIPSE_CLASSPATH="$1/eclipse/Client/.classpath"
+ECLIPSE_CLASSPATH="$1/eclipse/Client/.classpath"
+ECLIPSE_COREPREFS="$1/eclipse/Client/.settings/org.eclipse.jdt.core.prefs"
+ECLIPSE_LIBRARIES="$1/jars/libraries"
+if [ -d "$ECLIPSE_LIBRARIES" ] && [ -f "$ECLIPSE_CLASSPATH" ] && [ -f "$ECLIPSE_COREPREFS" ]; then
+	MC_VERSION=$(find -L "$1/jars/versions/" -maxdepth 1 -mindepth 1 -name "of" -prune -o \( -type d -print \) | sed "s/^$1\/jars\/versions\///")
+	if [ "$MC_VERSION" == "" ]; then
+		echo "Couldn't find a minecraft version linked at $1/jars/versions/"
+		exit 2
+	fi
+	echo "Automatically Updating Eclipse .classpath and compiler settings."
 else
-	echo "Couldn't find $1/jars/libraries or $1/eclipse/Client/.classpath"
+	echo "Couldn't find $ECLIPSE_LIBRARIES or $ECLIPSE_CLASSPATH or $ECLIPSE_COREPREFS"
 	exit 2
 fi
 IFS=$'\n'
 get_libraries(){
-	MC_VERSION=$(find -L "$1/lib" -maxdepth 1 -mindepth 1 -name "of" -prune -o \( -type d -print \) | sed "s/^$1\/lib\///")
-	if [ "$MC_VERSION" == "" ]; then
-		echo "Couldn't find a minecraft version linked at $1/lib/"
-		exit 2
-	fi
 	echo \
 	"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"\
 	"<classpath>\n"\
@@ -34,10 +37,10 @@ get_libraries(){
 	"\t<classpathentry kind=\"lib\" path=\"jars/versions/$MC_VERSION/$MC_VERSION.jar\"/>\n"\
 	"\t<classpathentry kind=\"lib\" path=\"jars/libraries\"/>"
 	for library in $(
-		find "$1/jars/libraries" \
-		-type d -path "$1/jars/libraries/ca/weblite/java-objc-bridge/*" -prune -o \
-		-type d -path "$1/jars/libraries/org/lwjgl/lwjgl/*" -prune -o \
-		-type d -path "$1/jars/libraries/com/google/code/findbugs/*" -prune -o \
+		find "$ECLIPSE_LIBRARIES" \
+		-type d -path "$ECLIPSE_LIBRARIES/ca/weblite/java-objc-bridge/*" -prune -o \
+		-type d -path "$ECLIPSE_LIBRARIES/org/lwjgl/lwjgl/*" -prune -o \
+		-type d -path "$ECLIPSE_LIBRARIES/com/google/code/findbugs/*" -prune -o \
 		-type f | sed "s/^$1\///"
 		# find all libraries except select the directory of those which need natives and/or provide sources instead
 	);	do
@@ -83,6 +86,26 @@ get_libraries(){
 	echo "\t<classpathentry kind=\"output\" path=\"bin\"/>"
 	echo "</classpath>"
 }
+update_prefs(){
+	cat <<-HDEOF
+	eclipse.preferences.version=1
+	org.eclipse.jdt.core.compiler.codegen.inlineJsrBytecode=enabled
+	org.eclipse.jdt.core.compiler.codegen.methodParameters=do not generate
+	org.eclipse.jdt.core.compiler.codegen.targetPlatform=17
+	org.eclipse.jdt.core.compiler.codegen.unusedLocal=preserve
+	org.eclipse.jdt.core.compiler.compliance=17
+	org.eclipse.jdt.core.compiler.debug.lineNumber=generate
+	org.eclipse.jdt.core.compiler.debug.localVariable=generate
+	org.eclipse.jdt.core.compiler.debug.sourceFile=generate
+	org.eclipse.jdt.core.compiler.problem.assertIdentifier=error
+	org.eclipse.jdt.core.compiler.problem.enablePreviewFeatures=disabled
+	org.eclipse.jdt.core.compiler.problem.enumIdentifier=error
+	org.eclipse.jdt.core.compiler.problem.reportPreviewFeatures=warning
+	org.eclipse.jdt.core.compiler.release=disabled
+	org.eclipse.jdt.core.compiler.source=17
+	HDEOF
+}
 echo "INFO: Libraries Found:"
 echo -e "$(get_libraries "$1")" > "$ECLIPSE_CLASSPATH"
+echo "$(update_prefs)" > "$ECLIPSE_COREPREFS"
 exit 0
